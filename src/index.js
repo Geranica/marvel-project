f99dcd975e123a80bf829ebdd8c68c6c23fb846c
Valerij Melguj <vj.melguj@gmail.com>
2024-02-06 14:51:55 +0300

    random charachter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
 import "./style/style.scss";
 
-const marvelService = new MarvelService();
-marvelService.getAllCharacters().then (result => console.log (result));
-marvelService.getCharacter (1017295).then (result => console.log (result.data.results));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -5,10 +5,9 @@ class MarvelService {
   getResource = async (url) => {
     const res = await fetch(url);
 
-    if (!res.ok) {
+    if (!res || !res.ok) {
       throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
-
     return await res.json(); // вернется промис
   };
 
@@ -18,8 +17,22 @@ class MarvelService {
     );
   };
 
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  getCharacter = async (id) => {
+    const result = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(result);
+  };
+
+  _transformCharacter = (result) => {
+    return {
+      name: result.data.results[0].name,
+      description: result.data.results[0].description,
+      thumbnail:
+        result.data.results[0].thumbnail.path +
+        "." +
+        result.data.results[0].thumbnail.extension,
+      homepage: result.data.results[0].urls[0].url,
+      wiki: result.data.results[0].urls[1].url,
+    };
   };
 }
 
