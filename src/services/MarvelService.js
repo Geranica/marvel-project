2fdab310564cc1c5886f407d675eb472b808ea78
Valerij Melguj <vj.melguj@gmail.com>
2024-02-06 19:32:41 +0300

    add spinner and error message

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -5,35 +5,37 @@ class MarvelService {
   getResource = async (url) => {
     const res = await fetch(url);
 
-    if (!res || !res.ok) {
+    if (!res.ok) {
       throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
     return await res.json(); // вернется промис
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
-      `${this._apiBase}characters?limit=9&offset=200&${this._apiKey}`
-    );
+  getAllCharacters = async () => {
+    const result =  await this.getResource(`${this._apiBase}characters?limit=9&offset=200&${this._apiKey}`);
+    return result.data.results.map(this._transformCharacter);
+
+   //(char) => this._transformCharacter(char)
   };
 
   getCharacter = async (id) => {
     const result = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-    return this._transformCharacter(result);
+    return this._transformCharacter(result.data.results[0]);
   };
 
-  _transformCharacter = (result) => {
+  _transformCharacter = (char) => {
     return {
-      name: result.data.results[0].name,
-      description: result.data.results[0].description,
+      name: char.name,
+      description: char.description,
       thumbnail:
-        result.data.results[0].thumbnail.path +
+        char.thumbnail.path +
         "." +
-        result.data.results[0].thumbnail.extension,
-      homepage: result.data.results[0].urls[0].url,
-      wiki: result.data.results[0].urls[1].url,
+        char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
     };
   };
 }
 
 export default MarvelService;
+
