86ff6cd87ac0dd05cfed2420285362d86d2e224b
Valerij Melguj <vj.melguj@gmail.com>
2024-02-07 17:56:15 +0300

    chars and comics

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,29 +12,30 @@ class MarvelService {
   };
 
   getAllCharacters = async () => {
-    const result =  await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    const result = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
     return result.data.results.map((char) => this._transformCharacter(char));
   };
 
   getCharacter = async (id) => {
-    const result = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    const result = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
     return this._transformCharacter(result.data.results[0]);
   };
 
   _transformCharacter = (char) => {
     return {
-      id:char.id,
+      id: char.id,
       name: char.name,
       description: char.description,
-      thumbnail:
-        char.thumbnail.path +
-        "." +
-        char.thumbnail.extension,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
+      comics: char.comics.items,
     };
   };
 }
 
 export default MarvelService;
-
