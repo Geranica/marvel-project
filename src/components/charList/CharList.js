a9de466550be94940624d4a14cbda16410483fa9
Valerij Melguj <vj.melguj@gmail.com>
2024-02-12 17:14:13 +0300

    remake class components into func components

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { Component } from "react/cjs/react.production.min";
+import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 import MarvelService from "../../services/MarvelService";
@@ -8,65 +8,70 @@ import ErrorMessage from "../errorMessage/ErrorMessage";
 import "./charList.scss";
 //import abyss from "../../resources/img/abyss.jpg";
 
-class CharList extends Component {
-  state = {
-    chars: [],
-    loading: true,
-    error: false,
-    newItemLoading: false,
-    offset: 210,
-    charEnded: false,
-    selectedCharIndex: null,
+const CharList = (props) => {
+  const [chars, setChars] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [newItemLoading, setNewItemLoading] = useState(false);
+  const [offset, setOffset] = useState(210);
+  const [charEnded, setCharEnded] = useState(false);
+  const [selectedCharIndex, setSelectedCharIndex] = useState(null);
+
+  const marvelService = new MarvelService();
+
+  const onRequest = (offset) => {
+    onCharListLoading();
+    marvelService.getAllCharacters(offset).then(onCharsLoaded).catch(onError);
   };
 
-  marvelService = new MarvelService();
+  useEffect(() => {
+    onRequest();
+  }, []);
 
-  handleClick(charId) {
-    this.props.onCharSelected(charId);
-    this.setState({ selectedCharIndex: charId });
-  }
-
-  componentDidMount() {
-    this.onRequest();
-  }
-
-  onRequest = (offset) => {
-    this.onCharListLoading();
-    this.marvelService
-      .getAllCharacters(offset)
-      .then(this.onCharsLoaded)
-      .catch(this.onError);
+  const onCharListLoading = () => {
+    setNewItemLoading(true);
   };
 
-  onCharListLoading = () => {
-    this.setState({ newItemLoading: true });
-  };
-
-  onCharsLoading = () => {
-    this.setState({ loading: true });
-  };
-  onCharsLoaded = (newChars) => {
+  const onCharsLoaded = (newChars) => {
     let ended = false;
 
     if (newChars < 9) {
       ended = true;
     }
-
-    this.setState(({ chars, offset }) => ({
-      chars: [...chars, ...newChars],
-      loading: false,
-      newItemLoading: false,
-      offset: offset + 9,
-      charEnded: ended,
-    }));
+    setChars((chars) => [...chars, ...newChars]);
+    setLoading(false);
+    setNewItemLoading((newItemLoading) => false);
+    setOffset((offset) => offset + 9);
+    setCharEnded((charEnded) => ended);
   };
 
-  onError = () => {
-    this.setState({ loading: false, error: true });
+  const onError = () => {
+    setLoading(false);
+    setError(true);
   };
 
-  renderItems(arr) {
-    const items = arr.map((item) => {
+  /* const focusInItem = (id) => {
+    itemRefs.current.forEach(item => {
+      item.classList.remove('char__item_selected')
+    });
+    itemRefs.current[id].classList.add('char__item_selected')
+    itemRefs.current[id].focus()
+  }
+
+  const itemRefs = useRef([]);
+    */
+
+  const handleClick = (charId) => {
+    props.onCharSelected(charId);
+    setSelectedCharIndex(charId);
+  };
+
+  /* const onCharsLoading = () => {
+    setLoading(true);
+  }; */
+
+  function renderItems(arr) {
+    const items = arr.map((item, i) => {
       let imgClassName = "";
       let charCardClassName = "char__item";
 
@@ -74,15 +79,16 @@ class CharList extends Component {
         imgClassName += "char__item img dynamic-fit-unset";
       }
 
-      if (item.id === this.state.selectedCharIndex) {
+      if (item.id === selectedCharIndex) {
         charCardClassName += " char__item_selected";
       }
 
       return (
         <li
+          /*  ref={el => itemRefs.current[i] = el} */
           className={charCardClassName}
           key={item.id}
-          onClick={() => this.handleClick(item.id)}
+          onClick={() => handleClick(item.id)}
         >
           <img src={item.thumbnail} alt={item.name} className={imgClassName} />
           <div className="char__name">{item.name}</div>
@@ -93,33 +99,28 @@ class CharList extends Component {
     return <ul className="char__grid">{items}</ul>;
   }
 
-  render() {
-    const { chars, loading, error, newItemLoading, offset, charEnded } =
-      this.state;
+  const items = renderItems(chars);
 
-    const items = this.renderItems(chars);
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || error) ? items : null;
 
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? items : null;
-
-    return (
-      <div className="char__list">
-        {errorMessage}
-        {spinner}
-        {content}
-        <button
-          style={{ display: charEnded ? "none" : "block" }}
-          onClick={() => this.onRequest(offset)}
-          disabled={newItemLoading}
-          className="button button__main button__long"
-        >
-          <div className="inner">load more</div>
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="char__list">
+      {errorMessage}
+      {spinner}
+      {content}
+      <button
+        style={{ display: charEnded ? "none" : "block" }}
+        onClick={() => onRequest(offset)}
+        disabled={newItemLoading}
+        className="button button__main button__long"
+      >
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
+};
 
 CharList.propTypes = {
   onCharSelected: PropTypes.func,
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,77 +1,70 @@
-import { Component } from "react/cjs/react.production.min";
+import { useState, useEffect } from "react";
+
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
+
 import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
 
-class RandomChar extends Component {
-  state = {
-    char: {},
-    loading: true,
-    error: false,
-  };
+const RandomChar = () => {
+  const [char, setChar] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  timerId = null;
+  const marvelService = new MarvelService();
 
-  marvelService = new MarvelService();
-
-  componentDidMount() {
-    this.updateChar();
-    //this.timerId = setInterval(this.updateChar, 3000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerId);
-  }
-
-  onCharLoading = () => {
-    this.setState({ loading: true });
-  };
-  onCharLoaded = (char) => {
-    this.setState({ char, loading: false });
-  };
-
-  onError = () => {
-    this.setState({ loading: false, error: true });
-  };
-
-  updateChar = () => {
-    this.onCharLoading();
+  const updateChar = () => {
+    onCharLoading();
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    this.marvelService
-      .getCharacter(id)
-      .then(this.onCharLoaded)
-      .catch(this.onError);
+    marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+  };
+  useEffect(() => {
+    updateChar();
+    const timerId = setInterval(updateChar, 60000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  const onCharLoading = () => {
+    setLoading(true);
   };
 
-  render() {
-    const { char, loading, error } = this.state;
+  const onCharLoaded = (char) => {
+    setChar(char);
+    setLoading(false);
+  };
 
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? <View char={char} /> : null;
+  const onError = () => {
+    setLoading(false);
+    setError(true);
+  };
 
-    return (
-      <div className="randomchar">
-        {errorMessage}
-        {spinner}
-        {content}
-        <div className="randomchar__static">
-          <p className="randomchar__title">
-            Random character for today!
-            <br />
-            Do you want to get to know him better?
-          </p>
-          <p className="randomchar__title">Or choose another one</p>
-          <button onClick={this.updateChar} className="button button__main">
-            <div className="inner">try it</div>
-          </button>
-          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
-        </div>
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || error) ? <View char={char} /> : null;
+
+  return (
+    <div className="randomchar">
+      {errorMessage}
+      {spinner}
+      {content}
+      <div className="randomchar__static">
+        <p className="randomchar__title">
+          Random character for today!
+          <br />
+          Do you want to get to know him better?
+        </p>
+        <p className="randomchar__title">Or choose another one</p>
+        <button onClick={updateChar} className="button button__main">
+          <div className="inner">try it</div>
+        </button>
+        <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 //Рендер-компонент без логики
 
@@ -80,12 +73,12 @@ const View = ({ char }) => {
   let imgClassName = "randomchar__img";
 
   if (/image_not_available/.test(thumbnail)) {
-    imgClassName += ' dynamic-fit-contain'
+    imgClassName += " dynamic-fit-contain";
   }
 
   return (
     <div className="randomchar__block">
-      <img  src={thumbnail} alt="Random character" className={imgClassName} />
+      <img src={thumbnail} alt="Random character" className={imgClassName} />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">
