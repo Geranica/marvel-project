f22856833be1316dacd5f2dc7db8651e41163510
Valerij Melguj <vj.melguj@gmail.com>
2024-02-20 18:20:04 +0300

    add lazy loading

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,17 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
+
+//import { MainPage, ComicsPage, SingleComicPage } from "../pages";
 
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
+
+ //выношу в импорт по умолчанию для ленивой подгрузки
+ //динамические импорты должны быть ниже статических
+const Page404 = lazy(() => import("../pages/404"));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
   return (
@@ -9,12 +19,16 @@ const App = () => {
       <div className="app">
         <AppHeader />
         <main>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/comics" element={<ComicsPage />} />
-            <Route path="/comics/:comicId" element={<SingleComicPage />} />
-            <Route path="*" element={<Page404 />} />
-          </Routes>
+          {/* для обработки ошибок при ленивой загрузке */}
+          {/* fallback выводит компонент, пока подгружается lazy компонент*/}
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/comics" element={<ComicsPage />} />
+              <Route path="/comics/:comicId" element={<SingleComicPage />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
     </Router>
